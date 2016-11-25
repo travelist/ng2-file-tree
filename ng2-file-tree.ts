605844d128bc57152dc10466fa6ebc55787efc4e
Komei Shimamura <komei.t.f@gmail.com>
2016-11-25 22:38:59 +0000

    Fix import/export error

--- ng2-file-tree.ts
+++ ng2-file-tree.ts
@@ -1,2 +1,4 @@
-export * from './src'
+export { FileType } from './src/tree-node'
+export { TreeNode } from './src/tree-node'
+export { TreeNodeParams } from './src/tree-node'
 export { Ng2FileTreeModule } from './src/index'
